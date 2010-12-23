30637b53629b40844c9400e32ed0bb5c942e7106
Nick Peters <freon27@gmail.com>
2010-12-23 10:54:38 +0000

    Added stylesheeets

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe Project do
     it "should not be valid without a name" do
-      Project.new.should_not be_valid
+      @project = Project.new
+      @project.should_not be_valid
+      @project.name = 'Project 1'
+      @project.should be_valid
     end
 end
