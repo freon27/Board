1eba386f03739e850dee5305ae459ab0d6281730
Nick Peters <freon27@gmail.com>
2011-01-03 20:37:11 +0000

    Added password fields to user create view

--- app/models/resolution.rb
+++ app/models/resolution.rb
@@ -1,11 +1,14 @@
 class Resolution < ActiveRecord::Base
-  attr_accessible :title, :description, :start_date, :end_date
+  attr_accessible :title, :description, :start_date, :end_date, :period
   belongs_to :user, :foreign_key => "owner"
   validates :title, :presence => true
   validates :start_date, :presence => true
   validates :end_date, :presence => true
+  validates :period, :presence => true
+  validate :date_validate
+  validates_inclusion_of :period, :in => [:weekly, :monthly, :daily, :once]
 
-  def validate  
+  def date_validate  
     errors.add_to_base "End date must be in the future" if  end_date && end_date < Date.today
     if(end_date && start_date && end_date < start_date) 
       errors.add_to_base "End date must be later than start date"
--- spec/models/resolution_spec.rb
+++ spec/models/resolution_spec.rb
@@ -7,7 +7,8 @@ describe Resolution do
       :title => 'Sample resolution 1',
       :description => 'This is a sample description',
       :start_date => Date.today + 1 ,
-      :end_date => Date.today + 2
+      :end_date => Date.today + 2,
+      :period => :weekly
     }
   end
   describe "creation" do
@@ -46,6 +47,29 @@ describe Resolution do
       resolution.should_not be_valid
       resolution.end_date = Date.today + 2
       resolution.should be_valid
+    end
+    it "should not be valid without period" do
+      @attr.delete(:period)
+      resolution = Resolution.new(@attr)
+      resolution.should_not be_valid
+      resolution.period = :weekly
+      resolution.should be_valid
+    end
+    it "should only be valid with an accepted period value" do
+      period_values = [
+        :daily,
+        :weekly,
+        :monthly,
+        :once
+      ]
+      resolution = Resolution.new(@attr)
+      resolution.should be_valid
+      period_values.each do | period |
+        resolution.period = period
+        resolution.should be_valid
+      end
+      resolution.period = :biannual
+      resolution.should_not be_valid
     end
   end
 end
